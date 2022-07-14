511a8167d2626e53560f0eae6acedb1df84bd2c7
Dayana <81794832+TotallyNotDayana@users.noreply.github.com>
2022-07-13 22:38:27 -0400

    uploaded to github
    finally

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            pictureBox1.Show();
+        }
+    }
+}
